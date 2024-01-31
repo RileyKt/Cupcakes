1ed4c99aee852ec1cf6d20f54bf324ed0554d34b
Riley Kneen-Teed <kneenrt@gmail.com>
2024-01-31 10:14:51 -0400

    working on add cupcake page

---
+++ Cupcakes/Models/Cupcake.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+
+namespace Cupcakes.Models
+{
+    
+    // Cupcake model to store cupcake data in database.
+    public class Cupcake
+    {
+        public int CupcakeId { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string ImageFilename { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+
+        public double Price { get; set; }
+
+
+       /*
+        [DisplayName("Photo")]
+        public string PhotoFileName { get; set; } = string.Empty;*/
+    }
+}
---
+++ Cupcakes/Pages/AddNewCupcake.cshtml.cs
@@ -0,0 +1,65 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Cupcakes.Data;
+using Cupcakes.Models;
+using System.ComponentModel;
+
+namespace Cupcakes.Pages
+{
+    public class AddNewCupcakeModel : PageModel
+    {
+        private readonly ILogger<AddNewCupcakeModel> _logger;
+        private readonly IHostEnvironment _environment;
+
+        [BindProperty]
+        public Cupcake Cupcake { get; set; } = new();
+
+        /*[BindProperty]
+        [DisplayName("Upload Photo")]
+        public IFormFile FileUpload { get; set; }
+        */
+        public AddNewCupcakeModel(ILogger<AddNewCupcakeModel> logger, IHostEnvironment environment)
+        {
+            _logger = logger;
+            _environment = environment;
+        }
+
+        public void OnGet()
+        {
+        }
+
+        /*public IActionResult OnPost()
+        {
+            
+            // Validate input
+            
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            // Upload file to server
+
+            string filename = FileUpload.FileName;
+
+            // Update Pet object to include the photo filename
+            Pet.PhotoFileName = filename;
+
+            // Save the file
+            string projectRootPath = _environment.ContentRootPath;
+            string fileSavePath = Path.Combine(projectRootPath, "wwwroot\\uploads", filename);
+
+            // We use a "using" to ensure the filestream is disposed of when we're done with it
+            using (FileStream fileStream = new FileStream(fileSavePath, FileMode.Create))
+            {
+                FileUpload.CopyTo(fileStream);
+            }
+
+            // Save to database
+
+            DbContext.AddNewPet(Pet);
+
+            return RedirectToPage("Index");
+        }*/
+    }
+}
