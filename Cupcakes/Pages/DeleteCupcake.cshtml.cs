baba55482483aa27f8913619248fe45d9551690f
Riley Kneen-Teed <kneenrt@gmail.com>
2024-01-31 14:39:07 -0400

    still cant fix the delete null exception error

--- Cupcakes/Pages/DeleteCupcake.cshtml.cs
+++ Cupcakes/Pages/DeleteCupcake.cshtml.cs
@@ -1,26 +1,22 @@
-using Microsoft.AspNetCore.Hosting;
+
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Cupcakes.Data;
 using Cupcakes.Models;
-using System.IO;
-using System.ComponentModel;
+
 
 namespace Cupcakes.Pages
 {
     public class DeleteCupcakeModel : PageModel
     {
+        private readonly ILogger<AddNewCupcakeModel> _logger;
+
         private readonly IWebHostEnvironment _environment;
 
         [BindProperty(SupportsGet = true)]
         public int CupcakeId { get; set; }
-
-        [BindProperty]
         public Cupcake Cupcake { get; set; } = new();
 
-        [BindProperty]
-        [DisplayName("Upload Image")]
-        public IFormFile FileUpload { get; set; }
         public DeleteCupcakeModel(IWebHostEnvironment environment)
         {
             _environment = environment;
@@ -33,10 +29,8 @@ namespace Cupcakes.Pages
 
         public IActionResult OnPost()
         {
-            Cupcake = DbContext.GetCupcakeById(CupcakeId);
 
-           
-
+          
             // Deletes the image file
             string fileToDelete = Path.Combine(_environment.WebRootPath, "uploads", Cupcake.ImageFilename);
             if (System.IO.File.Exists(fileToDelete))
